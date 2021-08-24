41e0ce6fb3ac25727c7f69dfb91a04da1cc0606f
Carlos <contact@carloscolin.me>
2021-08-24 16:38:28 -0500

    Use swagger docs and declared body parser

--- api/index.js
+++ api/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
 
 const config = require('../config.js');
 const user = require('./components/user/network');
 
 const app = express();
 
+// Swagger Document
+const swagger_doc = require('./swagger.json')
+
+// Body Parser
+app.use(express.json())
+
 // Router
 app.use('/api/user', user);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagger_doc))
 
 app.listen( config.api.port, () => {
     console.log(`Listening on port ${config.api.port}`);
