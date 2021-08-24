3ce1374616fd31484b186f127f5f4c4d6a7394ca
Carlos <contact@carloscolin.me>
2021-08-24 16:39:36 -0500

    Added upsert and remove functions

--- api/components/user/controller.js
+++ api/components/user/controller.js
@@ -1,11 +1,41 @@
-const store = require('../../../store/dummy');
+const { nanoid } = require('nanoid');
 
-const TABLA = 'user';
+const TABLE = 'user';
 
-function list () {
-    return store.list(TABLA);
+module.exports = function (injected_store) {
+    let store = injected_store;
+    if (!store) {
+        store = require('../../../store/dummy');
+    }
+
+    function list () {
+        return store.list(TABLE);
+    }
+
+    function get (user_id) {
+        let id = parseInt(user_id);
+        return store.get(TABLE, id);
+    }
+
+    function upsert (body) {
+        const user = {
+            name: body.name
+        }
+
+        user.id = nanoid();
+
+        return store.upsert(TABLE, user);
+    }
+
+    function remove (body) {
+        return store.remove(TABLE, body.id);
+    }
+
+    return {
+        list,
+        get,
+        upsert,
+        remove
+    };
+
 }
-
-module.exports = {
-    list,
-};
