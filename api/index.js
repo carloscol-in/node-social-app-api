4e8bb28eb9c6f67cde6d785bb87e29f3cb5bf313
Carlos <contact@carloscolin.me>
2021-08-24 22:38:06 -0500

    Added errors middleware

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 
 const config = require('../config.js');
+const errors = require('../network/errors');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
 
@@ -17,6 +18,14 @@ app.use(express.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagger_doc))
 app.use('/api/user', user);
 app.use('/api/auth', auth);
+
+/**
+ * *********** *
+ * Middlewares *
+ * *********** *
+ */
+// Error middleware should be the last middleware declared
+app.use(errors);
 
 app.listen( config.api.port, () => {
     console.log(`Listening on port ${config.api.port}`);
