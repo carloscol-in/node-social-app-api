2b396f3ddd84abf1a0d48c3fafa4dd5fe4ce24b3
Carlos <contact@carloscolin.me>
2021-08-24 19:05:31 -0500

    Added function query to query object from db

--- store/dummy.js
+++ store/dummy.js
@@ -5,7 +5,7 @@ const db = {
 };
 
 async function list (table) {
-    return db[table];
+    return db[table] || [];
 }
 
 async function get (table, id) {
@@ -30,9 +30,19 @@ async function remove (table, id) {
     return db[table].splice(idx, 1);
 }
 
+async function query(table, q) {
+    let col = await list(table);
+    let keys = Object.keys(q);
+    let key = keys[0];
+
+    return col.filter( item => item[key] === q[key] )[0] || null;
+
+}
+
 module.exports = {
     list, 
     get, 
     upsert,
     remove,
+    query,
 }
