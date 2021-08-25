1bfc792f4a816601e90587ca821ebe00282555bf
Carlos <contact@carloscolin.me>
2021-08-24 21:05:01 -0500

    Added check obj, and getToken, decodeHeader & verify funcs

--- auth/index.js
+++ auth/index.js
@@ -1,7 +1,45 @@
 const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const SECRET = config.jwt.secret;
 
 function sign (data) {
-    return jwt.sign(data, 'secret');
+    return jwt.sign(data, SECRET);
+}
+
+function verify (token) {
+    return jwt.verify(token, SECRET);
+}
+
+const check = {
+    own: function (req, owner) {
+        // check the requesting user (by token) is the owner of the user
+        const decoded = decodeHeader(req);
+        console.log(decoded);
+    },
+}
+
+function getToken (auth) {
+    // Bearer {{token}}
+    if (!auth) {
+        throw new Error('No token found on request');
+    }
+
+    if (auth.indexOf('Bearer ') == -1) throw new Error('Format invalid');
+
+    let token = auth.replace('Bearer ', '');
+
+    return token;
+}
+
+function decodeHeader (req) {
+    const authorization = req.headers.authorization || '';
+    const token = getToken(authorization);
+    const decoded = verify(token);
+
+    req.user = decoded;
+
+    return decoded
 }
 
 module.exports = {
