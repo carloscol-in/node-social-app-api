1dc5d54be9ec65daac15153eefbd7deeaec23deb
Carlos <contact@carloscolin.me>
2021-08-24 19:01:41 -0500

    Added auth component to base router

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const swaggerUi = require('swagger-ui-express');
 
 const config = require('../config.js');
 const user = require('./components/user/network');
+const auth = require('./components/auth/network');
 
 const app = express();
 
@@ -13,8 +14,9 @@ const swagger_doc = require('./swagger.json')
 app.use(express.json())
 
 // Router
-app.use('/api/user', user);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swagger_doc))
+app.use('/api/user', user);
+app.use('/api/auth', auth);
 
 app.listen( config.api.port, () => {
     console.log(`Listening on port ${config.api.port}`);
